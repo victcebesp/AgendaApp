055e6d5a9d68496dcb0548a8defb3f262c6bbe2a
Víctor Ceballos Espinosa <victcebesp@gmail.com>
2017-02-16 17:29:16 +0000

    getters and setters added. Equals added

--- src/model/Contact.java
+++ src/model/Contact.java
@@ -2,10 +2,42 @@ package model;
 
 public abstract class Contact {
 
-    int telephone;
-    String email;
-    Address address;
+    private int telephone;
+    private String email;
+    private Address address;
 
     abstract String getName();
 
+    public int getTelephone() {
+        return telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setTelephone(int telephone) {
+        this.telephone = telephone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+    
+    @Override
+    public boolean equals(Object o){
+        Contact contact = (Contact) o;
+        return this.telephone == contact.telephone &&
+               this.email.equals(contact.email) &&
+               this.address.equals(contact.address);
+    }
+    
 }
